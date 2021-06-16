a244471c9c34e29ff227a551e9105f765e36dd7f
RyuJoonSik <71337000+RyuJoonSik@users.noreply.github.com>
2021-06-17 02:27:37 +0900

    Portfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+// var PHOTO = document.getElementsByClassName("photo")[0];
+// var PHOTO_LENGTH = Math.min(PHOTO.offsetWidth, PHOTO.offsetHeight);
+// PHOTO.style.width = PHOTO_LENGTH + "px";
+// PHOTO.style.height = PHOTO_LENGTH + "px";
+const BODY = document.getElementsByTagName("body")[0];
+
+BODY.style.width = window.innerWidth;
+BODY.style.height = window.innerHeight;
