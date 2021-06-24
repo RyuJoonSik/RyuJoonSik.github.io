b0685bfbc4bbb94ef497db0775612668297f9684
RyuJoonSik <71337000+RyuJoonSik@users.noreply.github.com>
2021-06-24 18:12:04 +0900

    Portfolio

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var BTN_TOP_POINT = window.innerHeight / 2;
-var BTN_TOP = document.getElementsByClassName('btn-top')[0];
+var BTN_TOP = document.getElementsByClassName("btn-top")[0];
 
-BTN_TOP.addEventListener('click', function(e){
+BTN_TOP.addEventListener("click", function (e) {
   e.preventDefault();
+
+  console.log("ho");
 
   window.scrollTo(0, 0);
 });
-
